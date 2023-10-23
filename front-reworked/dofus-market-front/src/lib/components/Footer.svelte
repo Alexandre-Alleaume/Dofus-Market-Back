3e1e4cd24a16fde59265b7599ae751d285b46f0e
Alexandre-Alleaume <117785761+Alexandre-Alleaume@users.noreply.github.com>
2023-10-24 00:35:01 +0200

    Delete front-reworked directory

--- front-reworked/dofus-market-front/src/lib/components/Footer.svelte
+++
@@ -1,8 +0,0 @@
-<div class="flex flex-col items-center text-xs border-t bg-surface-50 border-surface-500/10">
-	<p>Dofus-Exchange.com &copy; site conçu par Pumalicieux Serveur Galgarion</p>
-
-	<p>
-		Toutes les images sont la propriété d'Ankama Games®. Ce site n'est en aucun cas affilié à Ankama
-		Games®.
-	</p>
-</div>
